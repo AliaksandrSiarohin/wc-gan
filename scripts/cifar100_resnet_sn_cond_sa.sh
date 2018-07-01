56bef1b112350e143d2beb7268d1d2c203f62fa2
Your Name <you@example.com>
2018-07-01 03:14:11 +0200

    Add names to scripts.

--- scripts/cifar100_resnet_sn_cond_sa.sh
+++ scripts/cifar100_resnet_sn_cond_sa.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Maximum achieved at 50k
-python run.py --dataset cifar100 --generator_adversarial_objective hinge\
+name=$(basename $0)
+name=${name%.*}
+python run.py --name $name --dataset cifar100 --generator_adversarial_objective hinge\
  --discriminator_adversarial_objective hinge --generator_block_norm d --generator_block_after_norm ufconv\
  --generator_last_norm d --generator_last_after_norm uconv --discriminator_filters 256 --generator_filters 128\
  --discriminator_spectral 1 --lr_decay_schedule linear --number_of_epochs 50 --gan_type PROJECTIVE --filters_emb 10
--- scripts/cifar10_resnet_sn_uncond.sh
+++ scripts/cifar10_resnet_sn_uncond.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-python run.py --dataset cifar10 --generator_adversarial_objective hinge\
+name=$(basename $0)
+name=${name%.*}
+python run.py --name $name --dataset cifar10 --generator_adversarial_objective hinge\
  --discriminator_adversarial_objective hinge  --generator_block_norm d --generator_block_after_norm uconv\
  --generator_last_norm d --generator_last_after_norm uconv --discriminator_filters 128 --generator_filters 256\
  --discriminator_spectral 1 --lr_decay_schedule linear --number_of_epochs 50
