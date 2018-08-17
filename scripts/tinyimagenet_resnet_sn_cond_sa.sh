660aeef9e1485485964ccf13fc63932e5a99509d
Your Name <you@example.com>
2018-08-17 19:11:30 +0200

    Fix some scripts.

--- scripts/tinyimagenet_resnet_sn_cond_sa.sh
+++ scripts/tinyimagenet_resnet_sn_cond_sa.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 name=$(basename $0)
 name=${name%.*}
-python run.py --name $name --dataset tiny-imagenet --generator_adversarial_objective hinge\
+python run.py --phase test --name lel --dataset tiny-imagenet --generator_adversarial_objective hinge\
  --discriminator_adversarial_objective hinge  --generator_block_norm d --generator_block_after_norm ufconv\
  --generator_last_norm d --generator_last_after_norm uconv --discriminator_filters 1024 --generator_filters 128\
  --discriminator_spectral 1 --lr_decay_schedule linear --number_of_epochs 100 --gan_type PROJECTIVE --filters_emb 15
